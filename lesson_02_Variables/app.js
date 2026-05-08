a7bed61af9b4ed1a9cca562ae4a710a396525c5b
HansonJoel <joelhanson002@gmail.com>
2026-05-08 14:18:52 +0100

    variables

---
+++ lesson_02_Variables/app.js
@@ -0,0 +1,5 @@
+// Variables in typescript
+var num = 100;
+console.log(num);
+var str = "Hello World";
+console.log(str);
---
+++ lesson_02_Variables/app.ts
@@ -0,0 +1,6 @@
+// Variables in typescript
+let num = 100;
+console.log(num);
+
+const str = "Hello World";
+console.log(str);
