d020e76faf71445bb65eac8837f6d3627e5f59fb
HansonJoel <joelhanson002@gmail.com>
2026-05-08 14:38:25 +0100

    learnt about the basic datatypes

---
+++ 04_Datatypes/app.ts
@@ -0,0 +1,21 @@
+// Dataypes in Typescript
+
+// 1. String Datatype
+const str1 = "This is created using Single quote";
+const str2 = "This is created using double quote";
+const str3 = `This is created using backticks`;
+
+console.log(str1);
+console.log(str2);
+console.log(str3);
+
+// Number Datatype
+let num = 12;
+const pi = 3.14;
+
+// Boolean Datatype
+let isEligible = true;
+let isEqual = false;
+
+let isGreater = 10 < 15;
+console.log(isGreater);
