75ca7c88fba6fdefd7a5f36fa7c6e87969274f85
HansonJoel <joelhanson002@gmail.com>
2026-05-21 14:00:42 +0100

    learnt about array in typescript

---
+++ 07_Arrays/app.js
@@ -0,0 +1,11 @@
+// creeating arrays implicitly
+var person = ["john", 28, "male", 1000, true];
+var names = ["john", "mark", "steven"];
+var birthYear = [1998, 1994, 2005];
+// Creating arrays explicitly
+var firstName = [];
+firstName.push("eddy");
+console.log(firstName);
+var people = ["john", 28, "male", 1000];
+people.push("mark");
+console.log(people);
---
+++ 07_Arrays/app.ts
@@ -0,0 +1,14 @@
+// creeating arrays implicitly
+let person = ["john", 28, "male", 1000, true];
+let names = ["john", "mark", "steven"];
+let birthYear = [1998, 1994, 2005];
+
+// Creating arrays explicitly
+let firstName: string[] = [];
+firstName.push("eddy");
+console.log(firstName);
+
+let people: (string | number)[] = ["john", 28, "male", 1000];
+people.push("mark");
+
+console.log(people);
