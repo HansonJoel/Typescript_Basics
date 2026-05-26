14e585e7fd242ac15adb8b2a45103e00d21aa3d4
HansonJoel <joelhanson002@gmail.com>
2026-05-26 19:48:41 +0100

    I learnt about tuples

---
+++ 08_Tuples/app.js
@@ -0,0 +1,4 @@
+// let employee = [123, "john", 2000, true];   // This is just a normal array
+// Tuples
+var employee = [123, "john", 2000, true];
+console.log(employee);
---
+++ 08_Tuples/app.ts
@@ -0,0 +1,6 @@
+// let employee = [123, "john", 2000, true];   // This is just a normal array
+
+// Tuples
+let employee: [number, string, number, boolean] = [123, "john", 2000, true];
+
+console.log(employee);
