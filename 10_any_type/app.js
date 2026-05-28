97dfdefe8d1b3c6e9cbb6f4a7a4df76c6db6c549
HansonJoel <joelhanson002@gmail.com>
2026-05-28 20:34:52 +0100

    Learnt about any datatype in typescript

---
+++ 10_any_type/app.js
@@ -0,0 +1,8 @@
+var value;
+value = "Hello";
+value = 42;
+console.log(value);
+// We can also create an array contain a any datatype
+var arr;
+arr = ["Hello", 100, true, null, undefined];
+console.log(arr);
---
+++ 10_any_type/app.ts
@@ -0,0 +1,12 @@
+let value: any;
+value = "Hello";
+value = 42;
+
+console.log(value);
+
+// We can also create an array contain a any datatype
+
+let arr: any[];
+arr = ["Hello", 100, true, null, undefined];
+
+console.log(arr);
